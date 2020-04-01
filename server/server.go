6c6bb4af1354ec2a0b0b3440b2aa7c9344f96e59
Piyushhbhutoria <piyush.bhutoria98@gmail.com>
2020-04-01 13:54:28 +0530

    server fix

--- server/server.go
+++ server/server.go
@@ -5,5 +5,5 @@ import "github.com/decabits/vwo-golang-example-app/config"
 func Init() {
 	config := config.GetConfig()
 	r := NewRouter()
-	r.Run(config.GetString("server.port"))
+	r.Run(config.GetString("port"))
 }
